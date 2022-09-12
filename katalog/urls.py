9dd126223ea605e4af32491560fa6d15a53224e2
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-09-12 10:29:48 +0700

    first commit

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
 # TODO: Implement Routings Here
+
+from django.urls import path
+from katalog.views import show_catalog
+
+urlpatterns = [
+    path('', show_catalog, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+
+def show_catalog(request):
+    return render(request, "katalog.html", context)
+
+data_catalog = CatalogItem.objects.all()
+context = {
+    'data_catalog': data_catalog,
+    'name': 'Muhammad Hilman Al Ayubi',
+    'npm': '2106706653'
+}
