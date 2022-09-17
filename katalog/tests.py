80bae1657e079622eaba58e0c42e1ee5ea71f488
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-09-17 22:52:52 +0700

    add test

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from katalog.views import show_catalog
 
-# Create your tests here.
+class TestUrls(TestCase):
+
+    def test_template(self):
+        client = Client()
+        response = client.get(reverse("katalog:show_catalog"))
+        url = reverse('katalog:show_catalog')
+        self.assertEquals(resolve(url).func, show_catalog)
+
+    def test_urls(self):
+        c = Client()
+        url = reverse('katalog:show_catalog')
+        response = c.get(url)
+        self.assertEquals(response.status_code, 200)
--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from mywatchlist.views import show_mywatchlist_html
+class TestUrls(TestCase):
 
-# Create your tests here.
+    def test_urls_html_http(self):
+        c = Client()
+        url = reverse('mywatchlist:show_mywatchlist_html')
+        response = c.get(url)
+        self.assertEquals(response.status_code , 200)
+        
+    # def test_urls_html(self):
+    #     c = Client()
+    #     url = reverse('mywatchlist:show_mywatchlist_html')
+    #     self.assertEquals(resolve(url).func, show_mywatchlist_html)
+
+    def test_urls_json_http(self):
+        c = Client()
+        url = reverse('mywatchlist:show_mywatchlist_json')
+        response = c.get(url)
+        self.assertEquals(response.status_code , 200)
+
+    def test_urls_xml_http(self):
+        c = Client()
+        url = reverse('mywatchlist:show_mywatchlist_xml')
+        response = c.get(url)
+        self.assertEquals(response.status_code , 200)
