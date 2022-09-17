8fc89d43b3d4573fbcb3951a236879941901754e
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-09-17 22:53:18 +0700

    add app_name

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from mywatchlist.views import show_mywatchlist, show_mywatchlist_json, show_mywatchlist_json_by_id, show_mywatchlist_xml, show_mywatchlist_xml_by_id, show_home
+from mywatchlist.views import show_mywatchlist_html, show_mywatchlist_json, show_mywatchlist_json_by_id, show_mywatchlist_xml, show_mywatchlist_xml_by_id, show_home
+
+app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_home, name='show_home'),
-    path('html/', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist_html, name='show_mywatchlist_html'),
     path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
     path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
     path('xml/<int:id>', show_mywatchlist_xml_by_id, name='show_mywatchlist_xml_by_id'),
