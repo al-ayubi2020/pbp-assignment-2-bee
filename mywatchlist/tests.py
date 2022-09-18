4f0e060a8e2ccb1de9af83aa5d9e391818530095
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-09-18 07:28:34 +0700

    refactor test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -3,25 +3,20 @@ from django.urls import reverse, resolve
 from mywatchlist.views import show_mywatchlist_html
 class TestUrls(TestCase):
 
+    def setUp(self):
+        self.c = Client()
+        self.url_html = reverse('mywatchlist:show_mywatchlist_html')
+        self.url_json = reverse('mywatchlist:show_mywatchlist_json')
+        self.url_xml = reverse('mywatchlist:show_mywatchlist_xml')
+
     def test_urls_html_http(self):
-        c = Client()
-        url = reverse('mywatchlist:show_mywatchlist_html')
-        response = c.get(url)
+        response = self.c.get(self.url_html)
         self.assertEquals(response.status_code , 200)
-        
-    # def test_urls_html(self):
-    #     c = Client()
-    #     url = reverse('mywatchlist:show_mywatchlist_html')
-    #     self.assertEquals(resolve(url).func, show_mywatchlist_html)
 
     def test_urls_json_http(self):
-        c = Client()
-        url = reverse('mywatchlist:show_mywatchlist_json')
-        response = c.get(url)
+        response = self.c.get(self.url_json)
         self.assertEquals(response.status_code , 200)
 
     def test_urls_xml_http(self):
-        c = Client()
-        url = reverse('mywatchlist:show_mywatchlist_xml')
-        response = c.get(url)
+        response = self.c.get(self.url_xml)
         self.assertEquals(response.status_code , 200)
