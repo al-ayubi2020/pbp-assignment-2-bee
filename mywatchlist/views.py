d625b0b2912928b58318d1de3cad3d78c7721464
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-10-09 08:46:49 +0700

    tes deploy

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -4,15 +4,35 @@ from django.http import HttpResponse
 from django.core import serializers
 
 def show_home(request):
+    watched = MyWatchListItem.objects.filter(watched=True).count()
+    not_watched = MyWatchListItem.objects.filter(watched=False).count()
+    message = ''
+    if (watched > not_watched):
+        message = "Selamat, kamu sudah banyak menonton!"
+    else:
+        message = "Wah, kamu masih sedikit menonton!"
+
+    message_context = {
+        'message': message
+    }
+
     return render(request, "home.html", message_context)
 
 def show_mywatchlist_html(request):
+    data_mywatchlist = MyWatchListItem.objects.all()
+    context = {
+        'data_mywatchlist': data_mywatchlist,
+        'name': 'Muhammad Hilman Al Ayubi',
+        'npm': '2106706653',
+    }
     return render(request, "mywatchlist.html", context)
 
 def show_mywatchlist_xml(request):
+    data_mywatchlist = MyWatchListItem.objects.all()
     return HttpResponse(serializers.serialize("xml", data_mywatchlist), content_type="application/xml")
 
 def show_mywatchlist_json(request):
+    data_mywatchlist = MyWatchListItem.objects.all()
     return HttpResponse(serializers.serialize("json", data_mywatchlist), content_type="application/json")
 
 def show_mywatchlist_xml_by_id(request, id):
@@ -22,22 +42,3 @@ def show_mywatchlist_xml_by_id(request, id):
 def show_mywatchlist_json_by_id(request, id):
     data_mywatchlist_by_id = MyWatchListItem.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data_mywatchlist_by_id), content_type="application/json")
-
-watched = MyWatchListItem.objects.filter(watched=True).count()
-not_watched = MyWatchListItem.objects.filter(watched=False).count()
-message = ''
-if (watched > not_watched):
-    message = "Selamat, kamu sudah banyak menonton!"
-else:
-    message = "Wah, kamu masih sedikit menonton!"
-
-message_context = {
-    'message': message
-}
-
-data_mywatchlist = MyWatchListItem.objects.all()
-context = {
-    'data_mywatchlist': data_mywatchlist,
-    'name': 'Muhammad Hilman Al Ayubi',
-    'npm': '2106706653',
-}
